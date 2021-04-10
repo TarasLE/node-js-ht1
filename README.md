dead57bc68bf6aac2339fb8ef063e5bbacc4e264
Taras Liuzniak <lyuznyaktaras@gmail.com>
2021-04-10 12:21:30 +0300

    int save
    tast 1.1; 1.2 done

--- README.md
+++
@@ -1,1 +0,0 @@
-# node-js-ht1
---
+++ contacts.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+const path = require('path')
+const generateId = require('shortid')
+
+const contactsPath = path.join(__dirname, './db/contacts.json')
+
+function listContacts() {
+    fs.readFile(contactsPath, 'utf8', (err, data) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        console.log(data)
+    })
+}
+
+function getContactById(contactId) {
+    fs.readFile(contactsPath, (err, data) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const contacts = JSON.parse(data)
+        contactId < 0 || contactId > contacts.length
+            ? console.log('check contactId and try again')
+            : console.log(contacts.find((contact) => contact.id === contactId))
+    })
+}
+
+function removeContact(contactId) {
+    fs.readFile(contactsPath, (err, data) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+
+        const newList = JSON.parse(data).filter(
+            (contact) => contact.id !== contactId
+        )
+
+        refreshList(newList)
+    })
+}
+
+function addContact(name, email, phone) {
+    fs.readFile(contactsPath, (err, data) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+        const newList = JSON.parse(data)
+
+        const newContact = {
+            id: generateId(),
+            name,
+            email,
+            phone,
+        }
+
+        newList.push(newContact)
+        refreshList(newList)
+    })
+}
+
+function refreshList(contacts) {
+    fs.writeFile(contactsPath, JSON.stringify(contacts), (err) => {
+        if (err) {
+            console.log(err)
+            return
+        }
+    })
+}
+// listContacts()
+// getContactById(3)
+// removeContact(3)
+// addContact('taras', 'taras@mail.com', '0967744558')
